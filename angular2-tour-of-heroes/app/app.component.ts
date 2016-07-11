b03b075e4d0803fc2fca23dcaf32df2a1254f1b1
Marcel Jacques Machado <marcel.machado@gvt.com.br>
2016-07-11 15:27:29 -0300

    Part 3 ready!

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'my-app.html',
+    styleUrls: ['my-app.css']
 })
 
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heroes';
+
+    selectedHero: Hero;
+
+    public heroes = HEROES;
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
+}
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
--- angular2-tour-of-heroes/systemjs.config.js
+++ angular2-tour-of-heroes/systemjs.config.js
@@ -2,44 +2,44 @@
  * System configuration for Angular 2 samples Adjust as necessary for your
  * application needs.
  */
-(function(global) {
+(function (global) {
 	// map tells the System loader where to look for things
 	var map = {
-		'app' : 'app', // 'dist',
-		'@angular' : 'node_modules/@angular',
-		'angular2-in-memory-web-api' : 'node_modules/angular2-in-memory-web-api',
-		'rxjs' : 'node_modules/rxjs'
+		'app': 'app', // 'dist',
+		'@angular': 'node_modules/@angular',
+		'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+		'rxjs': 'node_modules/rxjs'
 	};
 	// packages tells the System loader how to load when no filename and/or no
 	// extension
 	var packages = {
-		'app' : {
-			main : 'main.js',
-			defaultExtension : 'js'
+		'app': {
+			main: 'main.js',
+			defaultExtension: 'js'
 		},
-		'rxjs' : {
-			defaultExtension : 'js'
+		'rxjs': {
+			defaultExtension: 'js'
 		},
-		'angular2-in-memory-web-api' : {
-			main : 'index.js',
-			defaultExtension : 'js'
+		'angular2-in-memory-web-api': {
+			main: 'index.js',
+			defaultExtension: 'js'
 		},
 	};
-	var ngPackageNames = [ 'common', 'compiler', 'core', 'forms', 'http',
-			'platform-browser', 'platform-browser-dynamic', 'router',
-			'router-deprecated', 'upgrade', ];
+	var ngPackageNames = ['common', 'compiler', 'core', 'forms', 'http',
+		'platform-browser', 'platform-browser-dynamic', 'router',
+		'router-deprecated', 'upgrade',];
 	// Individual files (~300 requests):
 	function packIndex(pkgName) {
 		packages['@angular/' + pkgName] = {
-			main : 'index.js',
-			defaultExtension : 'js'
+			main: 'index.js',
+			defaultExtension: 'js'
 		};
 	}
 	// Bundled (~40 requests):
 	function packUmd(pkgName) {
 		packages['@angular/' + pkgName] = {
-			main : '/bundles/' + pkgName + '.umd.js',
-			defaultExtension : 'js'
+			main: '/bundles/' + pkgName + '.umd.js',
+			defaultExtension: 'js'
 		};
 	}
 	// Most environments should use UMD; some (Karma) need the individual index
@@ -48,8 +48,8 @@
 	// Add package entries for angular packages
 	ngPackageNames.forEach(setPackageConfig);
 	var config = {
-		map : map,
-		packages : packages
+		map: map,
+		packages: packages
 	};
 	System.config(config);
 })(this);
